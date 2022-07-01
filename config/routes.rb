67801e15fe213c48331a924e778901b685f0cf59
Asmenys <76791232+Asmenys@users.noreply.github.com>
2022-07-01 15:33:38 +0000

    updated registration view to include username, updated post creation page to include a form with error display

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
     # Redirests signing out users back to sign-in
     get "users", to: "devise/sessions#new"
   end
-  
+
   devise_for :users
+
+  resources :posts, only: [:new, :create, :index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
