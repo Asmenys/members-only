cd63820e9ee6a99a9aa9bd7b2472b788efc732c7
Asmenys <76791232+Asmenys@users.noreply.github.com>
2022-07-01 16:14:24 +0000

    Added foreign_key value to Posts allowing for them to store user_id, added a validation to post model, added a hidden field to new post form that stores current user id

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,6 @@ class Post < ApplicationRecord
     validates :title, presence: true
     validates :body, presence: true
     validates :body, length: {minimum: 10}
+    validates :user_id, presence: true
 
 end
