fb269de3daecc62829d9a32eaf09604fd3ff850d
Asmenys <76791232+Asmenys@users.noreply.github.com>
2022-07-01 17:22:35 +0000

    allows non-logged-in users to view posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-    before_action :authenticate_user!
+    before_action :authenticate_user!, only: [:create, :new]
 
     def new
         @post = Post.new
